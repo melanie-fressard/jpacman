ab1df543d460d5c01ded0a756cb6e03bfd9adcb0
Maelle <maellemarinier@outlook.com>
2025-02-09 14:02:13 -0500

    Ajout de tests constructeur et decrementLives

---
+++ src/test/java/nl/tudelft/jpacman/board/PlayerTest.java
@@ -0,0 +1,35 @@
+package nl.tudelft.jpacman.board;
+
+
+import nl.tudelft.jpacman.level.Player;
+import nl.tudelft.jpacman.level.PlayerFactory;
+import nl.tudelft.jpacman.sprite.PacManSprites;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PlayerTest {
+    PacManSprites SPRITE_STORE = new PacManSprites();
+    PlayerFactory playerFactory;
+    Player playerDefault;
+    Player playerCustom;
+    public void setUp() {
+        playerFactory = new PlayerFactory(SPRITE_STORE);
+        playerDefault = playerFactory.createPacMan();
+        playerCustom = playerFactory.createPacMan(10);
+    }
+    @Test
+    public void testCreatePacMan() {
+        assertThat( playerDefault.getLives()).isEqualTo(3);
+        assertThat( playerCustom.getLives()).isEqualTo(10);
+    }
+
+    @Test
+    public void testDecrementLives() {
+        playerDefault.decrementLives(1);
+        assertThat(playerDefault.getLives()).isEqualTo(2);
+        playerDefault.decrementLives(2);
+        assertThat(playerDefault.getLives()).isEqualTo(0);
+    }
+
+}
